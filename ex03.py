17d1601e405fb3331213ba5cc656306d427638d6
Michel <106674127+MichelZero@users.noreply.github.com>
2022-06-22 20:45:46 -0300

    Create ex03.py

---
+++ ex03.py
@@ -0,0 +1,11 @@
+#
+# par ou ímpar e imprima a mensagem respectiva.
+
+valor = int(input("informe um valor: ")) # entrada de dados
+
+# processamento
+if (valor % 2 == 0): # se o valor for par
+  print(f"{valor} é par")
+else: # se o valor for impar
+  print(f"{valor} é impar")
+
